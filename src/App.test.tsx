09fc1e47bb3efac08e5c440f9c74ab3f5e08075d
elahmo <ahmetnovalic@gmail.com>
2022-01-29 13:58:29 +0100

    update test suite

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App'
 
 test('renders App component', () => {
   render(<App />)
-  const linkElement = screen.getByText(/Not Wordle/)
+  const linkElement = screen.getByText('Wordle (BOS) 🇧🇦')
   expect(linkElement).toBeInTheDocument()
 })

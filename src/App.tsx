2f3c4324d835ea2debe447ecfeb0fdd8833a9fcf
elahmo <ahmetnovalic@gmail.com>
2022-02-04 21:46:08 +0100

    styling updates

--- src/App.tsx
+++ src/App.tsx
@@ -217,7 +217,7 @@ function App() {
 
       <button
         type="button"
-        className="mx-auto mt-4c flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 select-none"
+        className="mx-auto mt-4 flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 select-none"
         onClick={() => setIsAboutModalOpen(true)}
       >
         {ABOUT_GAME_MESSAGE}

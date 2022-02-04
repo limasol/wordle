f2177aa459903cc7c6668792254819d9f31e9d94
elahmo <ahmetnovalic@gmail.com>
2022-02-04 22:00:39 +0100

    update build, revert colours

--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -24,7 +24,7 @@ export const Key = ({
       'bg-slate-200 dark:bg-slate-600 hover:bg-slate-300 active:bg-slate-400':
         !status,
       'bg-slate-400 dark:bg-slate-800 text-white': status === 'absent',
-      'bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white':
+      'bg-green-500 hover:bg-green-600 active:bg-green-700 text-white':
         status === 'correct',
       'bg-yellow-500 hover:bg-yellow-600 active:bg-yellow-700 dark:bg-yellow-700 text-white':
         status === 'present',
